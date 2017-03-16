a8492a45653a442ac5b8cfa05ecedcfd7b014872
PhamVanHoan <hoan96nb@gmail.com>
2017-03-17 01:18:56 +0700

    sửa login và thêm logout cho admin

--- admin/admin.php
+++ admin/admin.php
@@ -1,3 +1,9 @@
+<?php 
+include('module/config.php');
+include('session.php');
+$result=mysqli_query($conn, "select * from admin where id='$session_id'")or die('Error In Session');
+$row=mysqli_fetch_array($result);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
