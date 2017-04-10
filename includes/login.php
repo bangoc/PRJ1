a74bc8d5102ac2870f219ce1bf2c4e73551cd1ab
BangPhamSy <bangphamsy97@gmail.com>
2017-04-11 03:57:14 +0700

    handle file log_in.php

--- includes/login.php
+++ includes/login.php
@@ -1,153 +1,116 @@
- <!-- Button -->
-  <script type="text/javascript">
-      $(document).ready(function(){
-         $('#login-btn').click(function(){
-            
-           // if($('#username').val()=='' || $('#password').val()==''){
-              //  $('.error').html("**Bạn vui lòng điền đầy đủ thông tin**");
-               // return false;
-           // }
-            //else
-            //{
-               // $('.error').addClass('hide');
-            
-            var data={
 
-                  username : $('#username').val(),
-                  password : $('#password').val()
-            };
-        
-           $.ajax({
-                 type    : 'post',
-                 dataType: 'json',
-                 url     : 'log_in.php',
-                 data    :  data,
-                 success : function(result){
-                   
-                  if (result.error == '1'){
-                     
-                      $('.alert-danger').html("Tài khoản hoặc mật khẩu không chính xác!").removeClass('hide');
-                      $('.alert-success').addClass('hide');
-                     }
-                   else
-                     {
-
-                        $('.alert-success').html("Đăng nhập thành công!").removeClass('hide');
-                        $('alert-danger').addClass('hide');
-                     }
-                }
-                 
-             
-            });
-         // }
-
-         });
-      });
-
- </script>
-
-  <!--   <script type="text/javascript">
-        $(document).ready(function(){
- 
-           $('#login-btn').click(function(){
- 
-           var data = {
-            username    : $('#username').val(),
-            password    : $('#password').val()
-        };
- 
-        
-        $.ajax({
-            type : "post",
-            dataType : "JSON",
-            url : "log_in.php",
-            data : data,
-            success : function(result)
-            {
-                
-                if (result.hasOwnProperty('error') && result.error == '1'){
-                    var html = '';
- 
-                    
-                    $.each(result, function(key, item){
-                        
-                        if (key != 'error'){ 
-                            html += '<li>'+item+'</li>';
-                        }
-                    });
-                    $('.alert-danger').html(html).removeClass('hide');
-                    $('.alert-success').addClass('hide');
-                }
-                else{ 
-                    $('.alert-success').html('Đăng nhập thành công!').removeClass('hide');
-                    $('.alert-primary').addClass('hide');
- 
-                 <!--   
-                    setTimeout(function(){
-                        $('#myModal').modal('hide');
-                        $('.alert-danger').addClass('hide');
-                        $('.alert-success').addClass('hide');
-                    }, 4000);
-                }
-            }
-        });
-    });
-});
-</script>-->
  <style type="text/css">
   .row{
     padding-top: 15px;
   }
 </style>
-           
-
-
-
-
-
-
+<body>
+   
            <a role="button" data-toggle="modal" data-target="#login"><i class="fa fa-user" aria-hidden="true"></i></i>Đăng nhập</a>
  
-            <!-- Modal -->
-            <div id="login" class="modal fade"  role="dialog">
-                <div class="modal-dialog">
+            
+            <div id="login" class="modal ">
+             
  
-                    <!-- Modal content-->
-                    <div class="modal-content" style="background:#BCF5A9;margin-top: 100px;">
+                    
+                    <div class="content" style="background:#BCF5A9;margin-top: 100px;width: 600px;
+                              margin-left: 400px;">
                         <div class="modal-header" style="background: #F781F3;">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title" style="color: #FF4000;text-align: center;">ĐĂNG NHẬP HỆ THỐNG</h4>
+                            <button type="modal-button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="title" style="color: #FF4000;text-align: center;">ĐĂNG NHẬP HỆ THỐNG</h4>
                         </div>
-                        <div class="modal-body">
+                      <div class="modal-body">
+                          
                             <div class="row">
-                                <div class="col-md-3"><label>Tên đăng nhập:</label></div>
-                                <div class="col-md-8">
-                                    <input class="form-control" type="text" id="username" />
+                                <div class="col-md-5"><label>Tên đăng nhập:</label></div>
+                                <div class="col-md-7">
+                                    <input class="form-control" type="text" id="username"/>
                                 </div>
+
                                 
                             </div>
                             
                             <div class="row">
-                                <div class="col-md-3"><label>Mật khẩu</label>:</div>
-                                <div class="col-md-8">
-                                    <input class="form-control" type="text" id="password" />
+                                <div class="col-md-5"><label>Mật khẩu</label>:</div>
+                                <div class="col-md-7">
+                                    <input class="form-control" type="password" id="password" />
                                 </div>
                                 
                             </div>
                             <div class="row">
-                                <div class="error" style="text-align: center;color: red;"></div>
-                            </div>
-                        </div>
-                        <div class="alert alert-danger  hide">
-                          
-                        </div>
-                        <div class="alert alert-success  hide">
- 
-                        </div>
+                                     <div id="error" style="text-align: center;color: red;">
+                                       
+                                        </div>
+                             </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-danger" id="login-btn">Đăng nhập</button>
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                         </div>
-                    </div>
+                       
+                        </div>
+                
+                    
                 </div>
             </div>
+ <script type="text/javascript">
+      $(document).ready(function(){
+         $('#login-btn').click(function(){
+            var username = $('#username').val();
+            var password = $('#password').val();
+            
+      
+            
+            if(username==''){
+              
+               $('#error').html("**Bạn vui lòng điền tài khoản**");
+              
+             
+            }
+            else if(password=='')
+            {
+
+               $('#error').html("**Bạn vui lòng điền mật khẩu**");
+               
+              
+            }
+
+            else
+           
+            {
+
+                var dataString = 'username='+ username +  '&password='+ password;
+              
+                 $.ajax({
+                       type    : "POST",
+                       url     : 'log_in.php',
+                       data    :  dataString,
+                       success : function(html){
+                          if(html=='false'){
+                            $('#error').html("Tên tài khoản hoặc mật khẩu không chính xác");
+                      
+                          }
+                          else if(html=='true')
+                          {
+                           window.location="items.php";
+
+
+                            setTimeout(function(){
+                                $('#myModal').modal('hide');
+                                $('.alert-danger').addClass('hide');
+                                $('.alert-success').addClass('hide');
+                            }, 3000);
+                                  }
+                        
+                       }
+                    });
+
+
+              }
+
+             return false;
+         });
+     });
+
+ </script>
+</body>
+
--- signup.php
+++ signup.php
@@ -1,34 +1,32 @@
-<?php  // Biến trả kết quả về cho người dùng
-// dựa vào key error để nhận biết có lỗi hay không
-        $errors = array(
-            'error' => 0
+
+<?php  
+      $errors = array(
+         'error' => 0
         );
          
-        // BƯỚC 1: LẤY THÔNG TIN
+       
         $username   = isset($_POST['username']) ? trim($_POST['username']) : '';
         $password   = isset($_POST['password']) ? trim($_POST['password']) : '';
         $phone      = isset($_POST['email'])    ? trim($_POST['phone'])    : '';
         $address   = isset($_POST['address']) ? trim($_POST['address']) : '';
          
-        // BƯỚC 2: VALIATE THÔNG TIN ĐƠN GIẢN
+       
         if (empty($username)){
             $errors['username'] = 'Bạn chưa nhập tên đăng nhập';
         }
          
         if (empty($password)){
-            $errors['password'] = 'Bạn chưa nhập mật khẩu';
+           $errors['password'] = 'Bạn chưa nhập mật khẩu';
         }
-         
-         
-        // BƯỚC 3: KIỂM TRA CÓ LỖI KHÔNG, NẾU CÓ LỖI THÌ TRẢ VỀ LUÔN, CÒN KHÔNG THÌ TIẾP TỤC KIỂM TRA
+
         if (count($errors) > 1){
             $errors['error'] = 1;
-            die (json_encode($errors));
+           die (json_encode($errors));
         }
          
          
-        // BƯỚC 4: KẾT NỐI CSDL VÀ KIỂM TRA THÔNG TIN
-        $conn = mysqli_connect('localhost', 'root', '', 'webbangiay');
+       
+       $conn = mysqli_connect('localhost', 'root', '', 'webbangiay');
         if (!$conn){
             $errors['connect_db'] = 'Không thể kết nối đến database';
         }
@@ -42,17 +40,17 @@
         {
             $row = mysqli_fetch_assoc($result);
             if ($row['username'] == $username){
-                $errors['username'] = 'Tên đăng nhập đã tồn tại';
+               $errors['username'] = 'Tên đăng nhập đã tồn tại';
             }
         }
          
-        // BƯỚC 5: TRẢ KẾT QUẢ VỀ NẾU CÓ LỖI
+    
         if (count($errors) > 1){
             $errors['error'] = 1;
             die (json_encode($errors));
         }
          
-        // BƯỚC 6: LƯU VÀO CSDL
+         
         $sql = "INSERT INTO user(username, password, phone, address)".
                 " VALUES('$username','$password','$phone','$address')";
         if (!mysqli_query($conn, $sql)){
@@ -60,6 +58,6 @@
             $errors['sql_db'] = 'Lỗi câu truy vấn SQL';
         }
          
-        // Trả kết quả cuối cùng
+      
         die (json_encode($errors));
 ?>
