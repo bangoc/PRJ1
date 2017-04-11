68dd61c606aaa4fcdd1a72b9f455b43db9cad48a
BangPhamSy <bangphamsy97@gmail.com>
2017-04-11 22:09:11 +0700

    thêm trang female.php và male.php

---
+++ female.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php' ?>
+   <div class="container" style="margin-top: 70px;">
+     <?php include 'includes/side_bar.php' ?>
+   <div class="panel panel-success  col-sm-9" style="background: #FFFF00;"><h4 style="text-align: center;">Giày Nữ</h4></div>
+       <div class="content col-sm-9">
+            <div class="row">
+               
+       	    </div>
+       </div>
+</div>
+<?php include 'includes/footer.php' ?>
---
+++ male.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php' ?>
+   <div class="container" style="margin-top: 70px;">
+   <?php include 'includes/side_bar.php' ?>
+
+   <div class="panel panel-success col-sm-9" style="background: #FF4000;"><h4 style="text-align: center;">Giày Nam</h4></div>
+       <div class="content col-sm-9">
+            <div class="row">
+              
+       	    </div>
+       </div>
+</div>
+<?php include 'includes/footer.php' ?>
