58195277e44db0f4b2edbb5f5509f35e9b3e867f
BangPhamSy <bangphamsy97@gmail.com>
2017-04-23 23:07:09 +0700

    thêm trang user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
     <?php include 'includes/nav_user.php' ?>
          <div class="container" style="margin-top: 70px;margin-left: 89.5px;">
      <?php include 'includes/slide_show.php';
-           include 'includes/side_bar.php'; 
+           include 'user_client/user_sidebar.php'; 
            include 'lib/conn.php';
            include 'includes/top_hot.php';
       ?>
