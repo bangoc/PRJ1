d96beb67e207442e5c09add2d36632f07a004ed8
BangPhamSy <bangphamsy97@gmail.com>
2017-04-24 00:17:12 +0700

    fix site user.php

---
+++ user_client/nav_user.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<?php
+   session_start();
+
+?>
+<html>
+<head>
+	<title>Trang web ban laptop</title>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../bootstrap3/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap3/css/main.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap3/css/footer.css">
+	<link rel="stylesheet" type="text/css" href="../library/font-awesome/css/font-awesome.css">
+	<script src="../bootstrap3/js/jquery-3.1.1.min.js"></script>
+	<script src="../bootstrap3/js/bootstrap.js"></script>
+
+</head>
+<body>
+<nav class="navbar navbar-inverse" style="position: fixed;
+        z-index: 100;
+         margin-top: -70px;background: #ed1c24;border:none;">
+       <div class="container nav">
+		  <div class="container-fluid">
+		    <ul class="nav navbar-nav col-md-7">
+		      <li class="active col-md-3">
+		         <a href="user.php" style="background:#ed1c24; "><i class="fa fa-home" aria-hidden="true"></i>Trang chủ</a>
+		       </li>
+		      <li class="active col-sm-3">
+		         <a href="#" style="background:#ed1c24; "><i class="fa fa-question-circle"></i>Liên hệ</a>
+		      </li>
+
+               <li class="active col-md-6">
+                  <form class="navbar-form col-sm-6">
+			      <div class="input-group">
+			        <input type="text" class="form-control" placeholder="Search">
+			        <div class="input-group-btn">
+			          <button class="btn btn-default" type="submit">
+			            <i class="glyphicon glyphicon-search"></i>
+			          </button>
+			        </div>
+			      </div>
+           </form>
+               </li>
+		       
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right col-md-5">
+		       <li class="col-md-3" style="height: 100%;width: 40%;">
+		        <a href="cart.php"><i class="fa fa-cart-plus" aria-hidden="true"></i>Giỏ hàng(0) </a>
+		       </li>
+		      <li class="col-md-3" style="height: 100%;width: 20%;margin-top: 15px;
+                color: white;">
+                  <i class="fa fa-user-o" aria-hidden="true"></i>
+		         <?php echo $_SESSION['username']; ?></li>
+		      <li class="col-md-6" style="height: 100%;width: 40%;"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+		       </div>
+		    </ul>
+		  </div>
+		</div>
+</nav>
---
+++ user_client/user_slideshow.php
@@ -0,0 +1,57 @@
+<div class="row" style="margin-bottom: 100px;">
+       <div class="sidebar_left col-sm-3">
+          <img  src="../image/ad.jpg" style="width: 260px;height: 329px;">
+		 </div>
+
+	    <div id="myCarousel" class="carousel slide col-sm-6" data-ride="carousel" style="margin: auto;height: 329px;">
+						  <!-- Indicators -->
+						  <ol class="carousel-indicators">
+						    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						    <li data-target="#myCarousel" data-slide-to="1"></li>
+						    <li data-target="#myCarousel" data-slide-to="2"></li>
+						    <li data-target="#myCarousel" data-slide-to="3"></li>
+						  </ol>
+
+						  <!-- Wrapper for slides -->
+								  <div class="carousel-inner" role="listbox" style="height: 329px;">
+										    <div class="item active">
+										      <img src="../image/Giay-Tommy-Hilfiger-ms-4078-0.jpg" alt="Chania" width="460" height="90">
+										    </div>
+
+										    <div class="item">
+										      <img src="../image/Giay-the-thao-nam-chinh-hang-MS-4240-0.jpg" alt="Chania" width="460" height="329">
+										    </div>
+
+										    <div class="item">
+										      <img src="../image/guoc.jpg" alt="Flower" width="460" height="345">
+										    </div>
+
+										  </div>
+
+										  <!-- Left and right controls -->
+										  <a class="left carousel-control" data-target="#myCarousel" role="button" data-slide="prev">
+										    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+										    <span class="sr-only">Previous</span>
+										  </a>
+										  <a class="right carousel-control" data-target="#myCarousel" role="button" data-slide="next">
+										    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+										    <span class="sr-only">Next</span>
+										  </a>
+		</div> <!--HET phan slideshow-->
+		<div class="col-sm-3" style="height: 329px;">
+              <ul class="list-group" >
+				        	<li class="list-group-item" style="text-align: center;">
+						    			<img src="../image/guoc.jpg" style="width: 50%;height: 100px;">
+						    			<div class="caption"><a href="#"></a>Tên sp</div>
+						    			<div class="caption"><a href="#">>>Mua ngay<<</a></div>
+						    </li>
+				        	<li class="list-group-item" style="text-align: center;">
+						    			<img src="../image/bot.jpg" style="width: 50%;height: 100px;">
+						    			<div class="caption"><a href="#"></a>Tên sp</div>
+						    			<div class="caption"><a href="#">>>Mua ngay<<</a></div>
+				        	</li>
+				        </ul>
+
+		</div>
+
+		</div>
