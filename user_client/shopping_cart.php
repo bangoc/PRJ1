d182eed596c656a27f08616227dd9a220430fa06
BangPhamSy <bangphamsy97@gmail.com>
2017-04-26 03:56:18 +0700

    sửa phần update số lượng trong giỏ hàng

--- user_client/shopping_cart.php
+++ user_client/shopping_cart.php
@@ -4,32 +4,48 @@
            include '../lib/conn.php';
            include 'user_sidebar.php';   ?>
            <div class="container col-sm-9" >
-             <form style="background: #D0F5A9">
+             <form style="background: #D0F5A9" method="post" action="../library/update.php">
              <div class="nav nav-success" style="background: #33b75d;"><h4 style="text-align: center;">Shopping cart</h4></div>
+
                  <table class="table table-hover">
 										    <thead>
 										      <tr>
-										        <th class="col-sm-1">ID</th>
-										        <th class="col-sm-4">Tensp</th>
+										        <th class="col-sm-1">STT</th>
+										        <th class="col-sm-3">Tên sản phẩm</th>
 										        <th class="col-sm-2">Hình ảnh  </th>
-										        <th class="col-sm-2">Số lượng</th>
-										        <th class="col-sm-2">Giá</th>
-										         <th class="col-sm-1">Xóa</th>
+										        <th class="col-sm-1">Số lượng</th>
+										        <th class="col-sm-1">Giá</th>
+										        <th class="col-sm-2">Tổng tiền </th>
+										        <th class="col-sm-1">Xóa</th>
 										      </tr>
 										    </thead>
 			             <?php 
-                                 $price_total=0;
+			              $price_total=0;
+			              $stt=1;
+			              if(!isset($_SESSION['cart']) or empty($_SESSION['cart'])){
+			              	   echo "Gio hang trong!";
+
+			              }
+                                 
 								foreach ($_SESSION['cart'] as $value) {
 									
 									  ?>
 									    
                                         <tbody>
 								         <tr>
-										           <td><?php echo $value['idsanpham'];?></td>
+										           <td><?php echo $stt;?></td>
 										           <td><?php echo $value['tensanpham'];?></td>
 										           <td><img src="../image/<?php echo $value['hinhanh'];?>" style="width: 100px;height: 50px;"></td>
-										           <td><?php echo $value['quantity'];?></td>
-										           <td><?php echo  $price=$value['gia']*$value['quantity'];?></td>
+										           <td>
+										              <input type="text"  name="<?php echo $value['idsanpham'];?>"
+										                     value="<?php echo $value['quantity'];?> "
+										                     style="width: 30px;"
+										              >
+										              
+										              	
+										           </td>
+										           <td><?php echo $value['gia'];?></td>
+										           <td><?php echo $value['gia']*$value['quantity'];?></td>
 										           <td>
 										                <a href="../library/delete_items.php?id=<?php echo $value['idsanpham'];?>" 
 										                    onclick= 'return confirm("Bạn có muốn xóa không?")'>
@@ -40,7 +56,9 @@
 							            
 								<?php
 								                    $price_total+=$value['gia']*$value['quantity'];
+								                    $stt++;
 								}
+						   
 								   
 			              ?>
 			                 <thead>
@@ -56,6 +74,7 @@
 
              </table>
              <div class="nav nav-success" style="float: right;">
+                  <input class="btn btn-primary" type="submit" name="ok" value="Cập nhật">
 	              <a href="user_order.php" class="btn btn-danger" id="register-btn">Đặt hàng ngay</a>
 	              <a href="user.php" type="button" class="btn btn-success" > Tiếp tục mua hàng </a>
               </div>
