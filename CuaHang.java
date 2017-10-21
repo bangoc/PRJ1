94b4e211e8a862653d306f32bd26888c540e0e04
Duyen97 <32337071+Duyen97@users.noreply.github.com>
2017-10-21 15:34:23 +0700

    Add files via upload
    class cuaHang

---
+++ CuaHang.java
@@ -0,0 +1,31 @@
+package model;
+
+public class CuaHang {
+  private int cacChiPhiKhac;
+  private String tenCuaHang;
+  private String diaChi;
+  
+  public int getcacChiPhiKhac() {
+    return cacChiPhiKhac;
+  }
+  
+  public void setcacChiPhiKhac(int cacChiPhiKhac) {
+    this.cacChiPhiKhac = cacChiPhiKhac;
+  }
+  
+  public String gettenCuaHang() {
+    return tenCuaHang;
+  }
+  
+  public void settenCuaHang(String tenCuaHang) {
+    this.tenCuaHang = tenCuaHang;
+  }
+  
+  public String getdiaChi() {
+    return diaChi;
+  }
+  
+  public void setdiaChi(String diaChi) {
+    this.diaChi = diaChi;
+  }
+}
