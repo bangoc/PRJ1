e64b61d746802e66d7d941356cf3165e9f28a46a
Duyen97 <32337071+Duyen97@users.noreply.github.com>
2017-10-21 16:00:26 +0700

    Add files via upload

---
+++ project1/src/model/ChiPhiKhac.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ChiPhiKhac {
+  private String tenChiPhi;
+  private int tien;
+  
+  public String gettenChiPhi() {
+    return tenChiPhi;
+  }
+  
+  public void setteChiPhi(String tenChiPhi) {
+    this.tenChiPhi = tenChiPhi;
+  }
+  
+  public int gettien() {
+    return tien;
+  }
+  
+  public void settien(int tien) {
+    this.tien = tien;
+  }
+
+}
