11f49b241eb5bd6343da92ff24f8eb2a17473d06
minh1996bk <minh1996bk@gmail.com>
2017-12-08 09:46:55 +0700

    thêm package chứa các lớp thao tác với csdl

---
+++ Project1/src/model/connectDatabase/ConnectDatabase.java
@@ -0,0 +1,30 @@
+package wrapper;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectDatabase {
+  
+  public static Connection createConnect() throws IOException, ClassNotFoundException, SQLException {
+    // doc du lieu tu file properties
+    Properties prop = new Properties();
+    prop.load(ConnectDatabase.class.getClassLoader()
+        .getResourceAsStream("jdbc.properties"));
+
+    String driverClass = prop.getProperty("MYSQLJDBC.driver");
+    String url = prop.getProperty("MYSQLJDBC.url");
+    String userName = prop.getProperty("MYSQLJDBC.username");
+    String password = prop.getProperty("MYSQLJDBC.password");
+    
+    Class.forName(driverClass);  
+    
+    // tao ket noi toi co so du lieu
+    Connection connect = DriverManager.getConnection(url,userName,password); 
+    return connect;
+ 
+  }
+ 
+}
