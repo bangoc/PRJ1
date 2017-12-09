b326dc86745f0329f982c26b17581e4684f119c8
minh1996bk <minh1996bk@gmail.com>
2017-12-09 21:46:18 +0700

    viet phuong thuong ket noi csdl luu thong tin nhan vien cap nhat

--- Project1/src/model/connectDatabase/ConnectEmployee.java
+++ Project1/src/model/connectDatabase/ConnectEmployee.java
@@ -56,7 +56,27 @@ public class ConnectEmployee {
         
     }
     
-    public void saveChangedEmployee(Employee employee) {
-        
+    public void saveChangedEmployee(Employee employee, String linkImage, Division division) throws IOException, ClassNotFoundException, SQLException {
+        String query = "update employee "
+                + "set name = ?, sex = ?, birth_date = ?, address = ?, phone_no = ?, "
+                + "coefficient_salary = ?, image = ?, division = ? where id = ?";
+        Connection con = ConnectDatabase.createConnect();
+        PreparedStatement ps = con.prepareStatement(query);
+
+        // set values
+        ps.setString(1, employee.getName());
+        ps.setString(2, employee.getGender().toString());
+        ps.setString(3, MyDate.formatDate(employee.getDateOfBirth()));
+        ps.setString(4, employee.getAddress());
+        ps.setString(5, employee.getPhoneNumber());
+        ps.setInt(6, employee.getCoefficientsSalary());
+
+        // get image as stream to save to database
+        FileInputStream fin=new FileInputStream(linkImage);  
+        ps.setBinaryStream(7,fin,fin.available());  
+        ps.setString(8, division.toString());
+        ps.setInt(9, employee.getEmployeeId());
+        // execute query
+        ps.executeUpdate();
     }
 }
