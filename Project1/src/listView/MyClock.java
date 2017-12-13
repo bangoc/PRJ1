5d49fb7f30b78f24595ca8424f620290440ba69e
minh1996bk <minh1996bk@gmail.com>
2017-12-13 18:52:13 +0700

    them lop dong ho

---
+++ Project1/src/listView/MyClock.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listView;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author leo
+ */
+public class MyClock extends Thread {
+    JLabel lblClock;
+    public MyClock(JLabel lblClock) {
+        this.lblClock = lblClock;
+    } 
+    
+    @Override
+    public void run() {
+        while(true) {
+            lblClock.setText(new Date().toString());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MyClock.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
